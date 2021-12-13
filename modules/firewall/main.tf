73076e24278e541a260908386e552568ce39e4f2
jpavord <58146067+jpavord@users.noreply.github.com>
2021-12-13 12:59:35 -0600

    Update main.tf
    addd rules for https and ssh

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -26,7 +26,26 @@ resource "google_compute_firewall" "allow-http" {
     protocol = "tcp"
     ports    = ["80"]
   }
+  
+  resource "google_compute_firewall" "allow-https" {
+  name    = "${local.network}-allow-https"
+  network = "${local.network}"
+  project = "${var.project}"
 
-  target_tags   = ["http-server"]
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+    resource "google_compute_firewall" "allow-ssh" {
+  name    = "${local.network}-allow-ssh"
+  network = "${local.network}"
+  project = "${var.project}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  target_tags   = ["bigsmart_api_server"]
   source_ranges = ["0.0.0.0/0"]
 }
