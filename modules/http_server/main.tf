a16b583da439a595b9c00a107e853d0428958c89
jpavord <58146067+jpavord@users.noreply.github.com>
2021-12-13 15:32:49 -0600

    Update main.tf

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -35,7 +35,7 @@ resource "google_compute_instance" "bs-api-server" {
          root /big_api;
          index index.html index.htm index.nginx-debian.html index.php;
          server_name _;
-         location / { try_files $uri $uri/ /index.php?args; add_header 'Access-Control-Allow-Origin' '*'; }
+         location / { try_files \$uri \$uri/ /index.php?args; add_header 'Access-Control-Allow-Origin' '*'; }
          location ~ \.php$ { include snippets/fastcgi-php.conf; fastcgi_pass unix:/var/run/php/php7.4-fpm.sock; }
 }" | sudo tee /etc/nginx/sites-available/default > /dev/null
 sudo touch /big_api/info.php
@@ -44,7 +44,6 @@ phpinfo();
 ?>" | sudo tee /big_api/info.php > /dev/null
 sudo touch /big_api/index.html
 echo "<html><body><h1>Environment: ${local.network}</h1></body></html>" | sudo tee /big_api/index.html > /dev/null
-sudo systemctl stop apache2
 sudo apt remove apache2 -y
 sudo service nginx restart
   EOF
