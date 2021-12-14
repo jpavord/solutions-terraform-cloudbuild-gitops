5a0ad9e794acb808af957eb0ffa29b8603ba4c85
jpavord <58146067+jpavord@users.noreply.github.com>
2021-12-14 17:31:12 -0600

    Update outputs.tf
    cambio de valor en salida de front

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -34,9 +34,9 @@ output "external_ip" {
 }
 
 output "instance_name_front" {
-  value = "${module.http_server.instance_name}"
+  value = "${module.front_server.instance_name}"
 }
 
 output "external_ip_front" {
-  value = "${module.http_server.external_ip}"
+  value = "${module.front_server.external_ip}"
 }
