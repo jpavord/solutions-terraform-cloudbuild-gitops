043f440adbec6eb8ad3b4eb8dda18abb15de762b
jpavord <58146067+jpavord@users.noreply.github.com>
2021-12-14 17:22:28 -0600

    Update outputs.tf
    se agregaron outputs para front server

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -25,10 +25,18 @@ output "firewall_rule" {
   value = "${module.firewall.firewall_rule}"
 }
 
-output "instance_name" {
+output "instance_name_api" {
   value = "${module.http_server.instance_name}"
 }
 
 output "external_ip" {
+  value = "${module.http_server.external_ip}"
+}
+
+output "instance_name_front" {
+  value = "${module.http_server.instance_name}"
+}
+
+output "external_ip_front" {
   value = "${module.http_server.external_ip}"
 }
