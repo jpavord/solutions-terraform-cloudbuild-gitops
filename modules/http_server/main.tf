4513fa2a42c4aa0be26607b667f9f5d5a45d43e6
jpablovargas <pablo.vargas@bigsmart.mx>
2021-12-20 00:04:35 -0600

    add external ip reservada y ssh al pipeline

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -65,6 +65,7 @@ echo "test123123123123" | tee ~/id_rsa >/dev/null
 
     access_config {
       # Include this section to give the VM an external ip address
+      nat_ip = "34.68.237.33"
     }
 
   }
